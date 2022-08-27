2969233ab5bc6dfefa8294d4001b3c4f930d838f
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2022-08-26 21:16:49 -0300

    Iniciando o uso de threads e subdivisao de tarefas com pthread, vamos comecar subdividindo as tarefas em muitas threads e vendo no que da.

--- main.c
+++ main.c
@@ -10,22 +10,29 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <process.h>
 #include <locale.h>
 #define TAMANHO 1000 //defini o tamanho maximo da string pois em C puro strings são vetores
 #define LIMITE 6 // Limite para dividirmos em substrings depois
 
 void maiusculo(char *stringIn,char *stringOut);// protótipo da função maiusculo.
+void* computa_thread(void* id_inicio_thread); // essa função fará as chamadas de cada thread.
 int main()
 {
 
     char stringEntrada[TAMANHO],stringSAIDA[TAMANHO];
     int i=0;
+    pthread_t threads_id[TAMANHO]; // esse vetor armazena o numero de ID de cada thread, em teoria uma thread pra cada caractere
+
+
     setlocale(LC_ALL,"Portuguese"); // Fução para fazer o windows ler caracteres unicode portugues
     printf("Digite uma palavra:\n");
     scanf("%[^\n]s",stringEntrada); // lê uma linha inteira ignorando os espaços
 
     maiusculo(stringEntrada,stringSAIDA);
     printf("A string digitada é :\n%s\n",stringSAIDA);
+
+    pthread_exit(NULL); //essa linha serve para fazer a função main esperar a execução da thread para encerrar.
     return 0;
 }
 //converte cada letra da string para maiusculo até atingir o TAMANHO ou encontrar o caractere de quebra de linha '\n'
