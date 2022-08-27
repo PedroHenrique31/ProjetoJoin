809092ce329d321945f73fd20b895761652dd93c
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2022-08-26 22:28:44 -0300

    Tinha me esquecido de limpar a memoria e a string estava com problemas para medir o tamanho da string, vida de dev em C ... mas agora temos uma funcao para calcular o tamanho da string.

--- main.c
+++ main.c
@@ -13,24 +13,32 @@
 #include <process.h>
 #include <locale.h>
 #define TAMANHO 1000 //defini o tamanho maximo da string pois em C puro strings são vetores
-#define LIMITE 6 // Limite para dividirmos em substrings depois
+#define LIMITE 3 // Limite para dividirmos em substrings depois
 
 void maiusculo(char *stringIn,char *stringOut);// protótipo da função maiusculo.
 void* computa_thread(void* id_inicio_thread); // essa função fará as chamadas de cada thread.
+int len(char *stringIN); // recebe uma string e retorna seu tamanho, para nós podermos dividir.
+
 int main()
 {
 
     char stringEntrada[TAMANHO],stringSAIDA[TAMANHO];
     int i=0;
     pthread_t threads_id[TAMANHO]; // esse vetor armazena o numero de ID de cada thread, em teoria uma thread pra cada caractere
-
-
     setlocale(LC_ALL,"Portuguese"); // Fução para fazer o windows ler caracteres unicode portugues
+    //incializa todas as posições com '\n' pra nao dar pau na hora de usar o len().
+    for(int j=0;j<TAMANHO;j++){
+        stringEntrada[j]='\n';
+    }
+
+
     printf("Digite uma palavra:\n");
     scanf("%[^\n]s",stringEntrada); // lê uma linha inteira ignorando os espaços
 
     maiusculo(stringEntrada,stringSAIDA);
-    printf("A string digitada é :\n%s\n",stringSAIDA);
+
+    //computa_thread(&threads_id[0]);
+    printf("A string digitada é :\n%s \ne seu tamanho é:%d\n",stringSAIDA,len(stringEntrada));
 
     pthread_exit(NULL); //essa linha serve para fazer a função main esperar a execução da thread para encerrar.
     return 0;
@@ -42,4 +50,21 @@ void maiusculo(char stringIn[TAMANHO],char stringOut[TAMANHO]){
         stringOut[i]=toupper(stringIn[i]);
         i++;
     }while((i<TAMANHO)&& (stringOut[i]!='\n'));
+}
+void* computa_thread(void* id_inicio_thread){
+    int id=*((int*) id_inicio_thread);
+
+    printf("Numero de id da thread %d\n",id);
+}
+int len(char stringIN[TAMANHO]){
+    int tam=0;
+    _Bool ehOfimLinha=(stringIN[tam]=='\n');
+    _Bool ehLimiteString=(tam==TAMANHO);
+
+    while(!ehLimiteString && !ehOfimLinha){
+        tam++;
+        ehOfimLinha=(stringIN[tam]=='\n');
+        ehLimiteString=(tam==TAMANHO);
+    }
+    return tam;
 }
