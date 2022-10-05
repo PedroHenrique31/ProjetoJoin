8da13749d0e6119b56d330a3922ffd7c01c431f9
Victor Furtado <68715303+Bestviest@users.noreply.github.com>
2022-10-05 20:07:40 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 /**
     Autores
-    Pedro Henrique Carneiro de Araújo 22108287
+    Pedro Henrique Carneiro de AraÃºjo 22108287
     Luiz Felipe Palharo 21908107
     Pedro Augusto Leite Prates 22008270
+    Victor Furtado Estrada 22006647
+   
 */
 
 
@@ -12,8 +14,8 @@
 #include <pthread.h>
 #include <process.h>
 #include <locale.h>
-#include <string.h> // possui funções para manipulação de strings.
-#include "OperaString.c" // arquivo comm as funções para maiuscular a string.
+#include <string.h> // possui funÃ§Ãµes para manipulaÃ§Ã£o de strings.
+#include "OperaString.c" // arquivo comm as funÃ§Ãµes para maiuscular a string.
 #include "ModuloThreads.h"
 
 int main()
@@ -22,21 +24,21 @@ int main()
     char stringEntrada[TAMANHO],stringSAIDA[TAMANHO];
 
     //pthread_t threads_id[TAMANHO]; // esse vetor armazena o numero de ID de cada thread, em teoria uma thread pra cada caractere
-    setlocale(LC_ALL,"Portuguese"); // Fução para fazer o windows ler caracteres unicode portugues
-    //incializa todas as posições com '\n' pra nao dar pau na hora de usar o len().
+    setlocale(LC_ALL,"Portuguese"); // FuÃ§Ã£o para fazer o windows ler caracteres unicode portugues
+    //incializa todas as posiÃ§Ãµes com '\n' pra nao dar pau na hora de usar o len().
     for(int j=0;j<TAMANHO;j++){
         stringEntrada[j]='\n';
     }
 
 
     printf("Digite uma palavra:\n");
-    scanf("%[^\n]s",stringEntrada); // lê uma linha inteira ignorando os espaços
+    scanf("%[^\n]s",stringEntrada); // lÃª uma linha inteira ignorando os espaÃ§os
 
     computa_tudo(stringEntrada,stringSAIDA);
 
     //computa_thread(&threads_id[0]);
-    printf("A string digitada é :\n%s \ne seu tamanho é:%d\n",stringSAIDA,len(stringEntrada));
+    printf("A string digitada Ã© :\n%s \ne seu tamanho Ã©:%d\n",stringSAIDA,len(stringEntrada));
 
-    pthread_exit(NULL); //essa linha serve para fazer a função main esperar a execução da thread para encerrar.
+    pthread_exit(NULL); //essa linha serve para fazer a funÃ§Ã£o main esperar a execuÃ§Ã£o da thread para encerrar.
     return 0;
 }
